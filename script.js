eb09f8d863a1a50aa37942b2085a238d18a240aa
Ben Lauderbaugh <benjlaud1@gmail.com>
2017-04-13 15:57:17 -0500

    Refactored script.js to named functions

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-$(function (){ // document ready
+$(document).ready(documentReady);
 
-  // Add a cube to the DOM of the color button clicked
+function documentReady (){ // document ready
   // Button press event listener
-  $('.color-button').on('click',function () {
-    var colorOfCube = $(this).data('color');
-    $('.container').append("<div class='color-cube " + colorOfCube + "'></div>");
-    // Update the counter for the current cube count
-    updateCounter(colorOfCube);
-  }); // end color-button on click
-
-  // Remove a cube that is selected from the DOM
+  $('.color-button').on('click', appendColorCube);
   // Cube clicked event listener
-  $('.container').on('click', '.color-cube', function () {
-    var selectedCube = $(this).attr('class').split(' ')[1];
-    $(this).remove();
-    // Update the counter for the current cube count
-    updateCounter(selectedCube);
-  }); // end container, color-cube on click
+  $('.container').on('click', '.color-cube', removeColorCube);
+} // end documentReady
 
-}); // end document ready
+// Add a cube to the DOM of the color button clicked
+function appendColorCube() {
+  var colorOfCube = $(this).data('color');
+  $('.container').append("<div class='color-cube " + colorOfCube + "'></div>");
+  // Update the counter for the current cube count
+  updateCounter(colorOfCube);
+} // end color-button on click
+
+// Remove a cube that is selected from the DOM
+function removeColorCube () {
+  var selectedCube = $(this).attr('class').split(' ')[1];
+  $(this).remove();
+  // Update the counter for the current cube count
+  updateCounter(selectedCube);
+} // end container, color-cube on click
 
 // function updateCounter - updates the cube count to current number of cubes
 function updateCounter (color) {
